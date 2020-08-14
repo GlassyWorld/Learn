0e31d3fed099aa95241318a669faa69504188b9f
GlassyWorld <15832827782@163.com>
2020-08-14 20:59:13 +0800

    笔记系统

---
+++ Program/Cpp/build/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../Test.exe"
+  "../Test.exe.manifest"
+  "../Test.pdb"
+  "../libTest.dll.a"
+  "CMakeFiles/Test.dir/Test.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
